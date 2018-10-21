32833ea77059eaa3330bdd2acc864cde54067e41
tbm0115 <tbm0115@gmail.com>
2018-10-21 16:05:20 -0700

    Improved styling of site.

--- PokeTools/routes/index.js
+++ PokeTools/routes/index.js
@@ -6,5 +6,17 @@ var router = express.Router();
 router.get('/', function (req, res) {
     res.render('index', { title: 'Home' });
 });
+/* GET about page. */
+router.get('/about', function (req, res) {
+  res.render('about', { title: 'About' });
+});
+/* GET privacy policy page. */
+router.get('/privacy', function (req, res) {
+  res.render('privacy', { title: 'Privacy Policy' });
+});
+/* GET contact page. */
+router.get('/contact', function (req, res) {
+  res.render('contact', { title: 'Contact Us' });
+});
 
 module.exports = router;
